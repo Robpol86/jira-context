eb165a2efef697857bf5d504b0a060515dd0a0e9
Robpol86 <robpol86@gmail.com>
2014-10-26 13:47:23 -0700

    More tests.

--- tests/test_jira_prompt_success.py
+++ tests/test_jira_prompt_success.py
@@ -12,14 +12,9 @@ _save_cookies = getattr(jira_context, '_save_cookies')
 _load_cookies = getattr(jira_context, '_load_cookies')
 
 
-def _prompt(func, prompt):
-    print(prompt)
-    return 'user' if func == raw_input else 'pass'
-
-
 @pytest.mark.httpretty
-def test_no_cookies(tmpdir, capsys):
-    jira_context._prompt = _prompt
+def test_no_cookies(tmpdir):
+    jira_context._prompt = lambda f, p: 'user' if f == raw_input else 'pass'
     JIRA.ABORTED_BY_USER = False
     JIRA.COOKIE_CACHE_FILE_PATH = str(tmpdir.join('.jira_session_json'))
     assert dict() == _load_cookies(JIRA.COOKIE_CACHE_FILE_PATH)
@@ -38,6 +33,36 @@ def test_no_cookies(tmpdir, capsys):
         assert getattr(j, '_JIRA__authenticated_with_password') is True
 
     assert dict(JSESSIONID='ABC123') == _load_cookies(JIRA.COOKIE_CACHE_FILE_PATH)
+
+
+@pytest.mark.httpretty
+def test_bad_cookies(tmpdir, capsys):
+    jira_context._prompt = lambda f, p: 'user' if f == raw_input else 'pass'
+    JIRA.ABORTED_BY_USER = False
+    JIRA.COOKIE_CACHE_FILE_PATH = str(tmpdir.join('.jira_session_json'))
+    _save_cookies(JIRA.COOKIE_CACHE_FILE_PATH, dict(JSESSIONID='ABC000'))
+
+    def first_session_callback(request, _, headers):
+        assert 'Authorization' not in request.headers
+        assert 'JSESSIONID=ABC000' == request.headers['Cookie']
+        return 401, headers, '{}'
+
+    def second_session_callback(request, _, headers):
+        assert 'user:pass' == base64.b64decode(request.headers['Authorization'].split(' ')[-1])
+        headers['Set-Cookie'] = 'JSESSIONID=ABC123; Path=/'
+        return 200, headers, '{}'
+
+    httpretty.register_uri(httpretty.GET, re.compile('.*/serverInfo'), body='{"versionNumbers":[6,4,0]}')
+    httpretty.register_uri(httpretty.GET, re.compile('.*/session'), body=first_session_callback)
+    httpretty.register_uri(httpretty.POST, re.compile('.*/session'), body=second_session_callback)
+
+    with JIRA() as j:
+        assert j.ABORTED_BY_USER is False
+        assert j.authentication_failed is False
+        assert getattr(j, '_JIRA__authenticated_with_cookies') is False
+        assert getattr(j, '_JIRA__authenticated_with_password') is True
+
+    assert dict(JSESSIONID='ABC123') == _load_cookies(JIRA.COOKIE_CACHE_FILE_PATH)
     stdout, stderr = capsys.readouterr()
-    assert 'JIRA username: \nJIRA password: \n' == stdout
+    assert '' == stdout
     assert '' == stderr
